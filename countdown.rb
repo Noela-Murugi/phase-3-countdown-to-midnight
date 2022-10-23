ab4bf9720063c56bf54872fba7d0577b46815096
Noela-Murugi <murugin254@gmail.com>
2022-10-23 05:08:32 +0300

    completed the assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
-#write your code here
-
-def countdown
+def countdown_with_sleep(num)
+  sleep(num)
 end
+
+def countdown(sec)
+  while sec>0
+      puts "#{sec} SECOND(S)!"
+      sec -=1
+      countdown_with_sleep(5)
+      # less number of seconds faster the loop
+  end
+  return "HAPPY NEW YEAR!"
+end
+countdown(12)
